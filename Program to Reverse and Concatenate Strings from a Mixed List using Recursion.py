33782d29414490a29855895ea19023a1e58bc373
Rushikesh Karpe <142646819+Rushikarpe45@users.noreply.github.com>
2025-07-06 19:03:07 +0530

    Create Program to Reverse and Concatenate Strings from a Mixed List using Recursion.py

---
+++ Program to Reverse and Concatenate Strings from a Mixed List using Recursion.py
@@ -0,0 +1,10 @@
+# input['hai',45,2+4j,'bye']
+# output['iahhai','eybbye']
+
+# def sam(s,out=[],i=0):
+#     if i>=len(s):
+#         return out
+#     if type(s[i])==str:
+#         out.append(s[i][::-1]+s[i])
+#     return sam(s,out,i+1)
+# print(sam(eval(input("enter your list:"))))
