c68fd3f4b371e9b8d219889b26b24b573fefaed7
Rushikesh Karpe <142646819+Rushikarpe45@users.noreply.github.com>
2025-07-06 19:04:33 +0530

    Create Sum of Integers in List.py

---
+++ Sum of Integers in List.py
@@ -0,0 +1,10 @@
+#program to find the sum of all the interger present in a given list
+
+
+# def sum_int(n,sum=0,i=0):
+#     if i>=len(n):
+#         return sum
+#     if type(n[i])==int:
+#         sum+=n[i]
+#     return sum_int(n,sum,i=i+1)
+# print(sum_int([10,'rushi','ketan',20]))
